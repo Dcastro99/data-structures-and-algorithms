50b4ee444de53dd262be19c03f980684cb2dca14
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-07-24 12:53:05 -0700

    finished testing

--- linked-list/src/LinkedList.test.ts
+++ linked-list/src/LinkedList.test.ts
@@ -5,5 +5,18 @@ describe("linked list", () => {
   it("runs my custom test", () => {
     const list: Collection<string> = new LinkedList<string>();
     expect(list).toBeDefined();
+
+    list.insert("Pippin");
+    list.insert("Sam");
+    list.insert("Merry");
+    list.insert("Frodo");
+
+
+    expect(list.includes("Pippin")).toBe(true);
+    expect(list.includes("Sam")).toBe(true);
+    expect(list.includes("Merry")).toBe(true);
+    expect(list.includes("Frodo")).toBe(true);
+    expect(list.includes("Bilbo")).toBe(false);
+    console.log(JSON.stringify(list));
   });
 });
